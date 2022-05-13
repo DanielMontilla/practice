bc394e19a09e8b4325d4ede127476bd8af0d8792
Daniel Montilla <montilla.d2000@gmail.com>
2022-05-13 18:56:06 -0400

    group anagram

---
+++ python/group_anagrams.py
@@ -0,0 +1,43 @@
+from typing import List
+
+# https://leetcode.com/problems/group-anagrams
+
+
+def groupAnagrams(strs: List[str]) -> List[List[str]]:
+
+    def isAnagram(n: str, m: str):
+        if len(n) != len(m):
+            return False
+
+        for letter in n:
+            if letter in m:
+                m = m.replace(letter, '', 1)
+            else:
+                return False
+
+        return True
+
+    arrs: List[List[str]] = [[]]
+
+    for s in strs:
+        for arr in arrs:
+            if len(arr) == 0:
+                arr.append(s)
+                arrs.append([])
+                break
+            elif isAnagram(s, arr[0]):
+                arr.append(s)
+                break
+
+    arrs.pop()
+
+    return arrs
+
+
+# Notes:
+""""
+    Runtime: 9643 ms, faster than 5.03% of Python3 online submissions for Group Anagrams.
+    Memory Usage: 17.1 MB, less than 96.63% of Python3 online submissions for Group Anagrams.
+    
+    Very slow... but super memory efficient
+"""
---
+++ python/main.py
@@ -0,0 +1,14 @@
+from group_anagrams import groupAnagrams
+
+
+def main():
+    # res = groupAnagrams([""])
+    # res = groupAnagrams(["hhhhu", "tttti", "tttit", "hhhuh", "hhuhh", "tittt"])
+    # res = groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
+    res = groupAnagrams(["ddddddddddg", "dgggggggggg"])
+
+    print(res)
+
+
+if __name__ == '__main__':
+    main()
