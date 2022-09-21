d9a48932c7c72b7c3d885b63449f8269b0d8564e
Daniel Montilla <daniel@Daniels-MacBook-Air.local>
2022-09-21 12:28:56 -0400

    backup

---
+++ longest_substring_without_repeating_characters/rust/src/main.rs
@@ -0,0 +1,16 @@
+fn length_of_longest_substring(s: String) -> i32 {
+    return 0;
+}
+
+fn main() {
+    // Inputs:
+    let _s_1 = "abcabcbb";
+    let _s_2 = "abcbrascba";
+    let _s_3 = "bbbbb";
+
+    let res_1 = length_of_longest_substring(_s_1);
+    let res_2 = length_of_longest_substring(_s_2);
+    let res_3 = length_of_longest_substring(_s_3);
+
+    println!("{res_1} ; {res_2} ; {res_3}");
+}
